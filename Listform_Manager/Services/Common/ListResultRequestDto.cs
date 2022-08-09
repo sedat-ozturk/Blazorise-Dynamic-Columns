151811328e3484c87d5ff886744713f5fa1d6153
Sedat ÖZTÜRK <gamebooker@gmail.com>
2022-08-09 12:40:01 +0300

    ListResultRequestDto classı eklendi

---
+++ Listform_Manager/Services/Common/ListResultRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.Application.Dtos;
+
+namespace Listform_Manager.Services.Common
+{
+    public class ListResultRequestDto : PagedAndSortedResultRequestDto
+    {
+        public int? FormId { get; set; }
+        public string UserName { get; set; }
+    }
+}
