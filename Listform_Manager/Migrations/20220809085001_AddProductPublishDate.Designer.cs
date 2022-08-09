0f965d2f4fb3bd57c1b7ae7ec63c7ec3806e32f2
Sedat ÖZTÜRK <gamebooker@gmail.com>
2022-08-09 12:42:56 +0300

    Publishdate sütununa eklendi.

--- Listform_Manager/Migrations/20220809085001_AddProductPublishDate.Designer.cs
+++ Listform_Manager/Migrations/20220809085001_AddProductPublishDate.Designer.cs
@@ -4,6 +4,7 @@ using Listform_Manager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,10 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Listform_Manager.Migrations
 {
     [DbContext(typeof(Listform_ManagerDbContext))]
-    partial class Listform_ManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220809085001_AddProductPublishDate")]
+    partial class AddProductPublishDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Listform_Manager/Migrations/20220809085001_AddProductPublishDate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Listform_Manager.Migrations
+{
+    public partial class AddProductPublishDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublishDate",
+                table: "Product",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublishDate",
+                table: "Product");
+
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    PublishDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+    }
+}
