0e590ac2cf000144a193a5261bf6a947abbc2f70
Sedat ÖZTÜRK <gamebooker@gmail.com>
2022-08-09 12:44:26 +0300

    Localizasyon dosyaları değiştirildi.

--- Listform_Manager/Services/AppService/ProductService.cs
+++ Listform_Manager/Services/AppService/ProductService.cs
@@ -16,13 +16,37 @@ namespace Listform_Manager.Services.AppService
             CreateUpdateProductDto>, 
         IProductService
     {
+        private readonly IRepository<Product, int> _repository;
+
         public ProductService(IRepository<Product, int> repository) : base(repository)
         {
+            _repository = repository;
+
             GetPolicyName = Listform_ManagerPermissions.Product.Products;
             GetListPolicyName = Listform_ManagerPermissions.Product.Products;
             CreatePolicyName = Listform_ManagerPermissions.Product.Create;
             UpdatePolicyName = Listform_ManagerPermissions.Product.Edit;
             DeletePolicyName = Listform_ManagerPermissions.Product.Delete;
+        }
+
+        public override async Task<PagedResultDto<ProductDto>> GetListAsync(PagedAndSortedResultRequestDto input)
+        {
+            await CheckGetListPolicyAsync();
+
+            var query = await CreateFilteredQueryAsync(input);
+            var totalCount = await AsyncExecuter.CountAsync(query);
+
+            query = ApplySorting(query, input);
+            //query = ApplyPaging(query, input);
+
+            var entities = await AsyncExecuter.ToListAsync(query);
+            var entityDtos = await MapToGetListOutputDtosAsync(entities);
+
+            return new PagedResultDto<ProductDto>
+                (
+                    totalCount,
+                    entityDtos
+                );
         }
     }
 }
